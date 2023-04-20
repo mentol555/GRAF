67e67bd18fb9b3e1831386140f950a67ebfa6ab6
Hosszú Szilárd <55355512+mentol555@users.noreply.github.com>
2023-04-20 16:09:47 +0300

    Beadando v2

--- hermit_erintok_beadando.cpp
+++ hermit_erintok_beadando.cpp
@@ -10,13 +10,14 @@
 
 using namespace std;
 
-GLuint vao1, vao2;
+GLuint vao1, vao2, vao3;
 GLuint pointsvao;
-GLuint vbo1, vbo2; // Hermit curves vbos
+GLuint vbo1, vbo2, vbo3; // Hermit curves vbos
 GLuint pointsvbo;
 
 GLfloat HermiteCurve[4 * N];
 GLfloat HermiteCurve2[4 * N]; // masodik hermit gorbe
+GLfloat HermiteCurve3[4 * N]; // harmadik gorbe
 
 GLint dragged = -1; // tarolja h melyik pontot fogtuk meg
 
@@ -28,17 +29,20 @@ GLfloat step = 1.0f / (N - 1); // t in [0,1]
 GLfloat Points[] = {
         -0.75f, 0.0f, 0.0f, // elso pontja
         -0.5f, 0.5f, 0.0f,  // elso pontja
-        0.10f, 0.0f, 0.0f, // elso es masodik kozos pontja
-        0.20, 0.5f, 0.0f,  // elso es masodik kozos pontja
-        0.40f, 0.25f, 0.0f, // masodik pontja
-        0.55f, -0.25f, 0.0f // masodik pontja
+        -0.25f, 0.0f, 0.0f, // elso es masodik kozos pontja
+        -0.20, -0.5f, 0.0f,  // elso es masodik kozos pontja
+        0.20f, 0.25f, 0.0f, // masodik es harmadik kozos pontja
+        0.25f, 0.0f, 0.0f, // masodik es harmadik kozos pontja
+        0.5f, -0.15f, 0.0f, // harmadik pontja
+        0.55f, 0.3f, 0.0f // harmadik pontja
 };
 GLfloat Vectors[] = {
     // R1(x,y) = P2x - P1x, P2y - P1y
-    Points[3] - Points[0], Points[4] - Points[1], 0.0f, // elso erintoje
+    Points[3*1] - Points[3*0], Points[3*1+1] - Points[3*0+1], 0.0f, // elso erintoje
     // R2(x,y) = P4x - P3x, P4y - P3y
-    Points[9] - Points[6], Points[10] - Points[7], 0.0f, // elso es masodik kozos erintoja
-    Points[15] - Points[12], Points[16] - Points[13], 0.0f // masodik erintoje
+    Points[3*3] - Points[3*2], Points[3*3+1] - Points[3*2+1], 0.0f, // elso es masodik kozos erintoja
+    Points[3*5] - Points[3*4], Points[3*5+1] - Points[3*4+1], 0.0f, // masodik es harmadik kozos erintoje
+    Points[3*7] - Points[3*6], Points[3*7+1] - Points[3*6+1], 0.0f // harmadik erintoje
 };
 
 // tavolsag negyzet kiszamolas
@@ -55,13 +59,67 @@ GLint getActivePoint(GLfloat* p, GLfloat sensitivity, GLfloat x, GLfloat y) {
     GLfloat		xNorm = -1 + x / (WIDTH / 2); // eger pozicio normalizalasa: 640x640-esbol [-1,1] rendszerbe
     GLfloat		yNorm = -1 + (HEIGHT - y) / (HEIGHT / 2);
 
-    for (GLint i = 0; i < 6; i++)
+    for (GLint i = 0; i < 8; i++)
         if (dist2_2d(p[i * 3], p[i * 3 + 1], xNorm, yNorm) < s) // itt szamoljuk a tav.negyzetet a pont ket koorinatajat(p[i*3], p[i*3+1] es a kurzor koordinataja kozott, es a sugarnegyzettel osszehasonl.)
             return i;
 
     return -1;
 }
-// 
+void calcFirst() {
+    // elso gorbe ujraszamolasa, P1 pontbol indul a gorbe
+    HermiteCurve[0] = Points[3*0];
+    HermiteCurve[1] = Points[3*0+1];
+    HermiteCurve[2] = 0.0f;
+    for (i = 1; i < (N - 1); i++) {
+        t = 0 + i * step;
+        // P1x * (...) + P3x * (...) + R1x * (...) + R2x * (...)
+        // P1 és P3 pontokon megy át a görbe, P2 és P4 csak az érintővektorok számolásához vannak
+        HermiteCurve[i * 3] = Points[3*0] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[3*2] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3*0] * (t * t * t - 2.0f * t * t + t) + Vectors[3*1] * (t * t * t - t * t);
+        HermiteCurve[i * 3 + 1] = Points[3*0+1] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[3*2+1] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3*0+1] * (t * t * t - 2.0f * t * t + t) + Vectors[3*1+1] * (t * t * t - t * t);
+        HermiteCurve[i * 3 + 2] = 0.0f;
+    }
+    // P3 pontba vegzodik a gorbe
+    HermiteCurve[(3 * N) - 3] = Points[3*2];
+    HermiteCurve[(3 * N) - 2] = Points[3*2+1];
+    HermiteCurve[(3 * N) - 1] = 0.0f;
+}
+void calcSecond() {
+    // masodik gorbe ujraszamolasa, P3 pontbol indul a gorbe
+    HermiteCurve2[0] = Points[3*2];
+    HermiteCurve2[1] = Points[3*2+1];
+    HermiteCurve2[2] = 0.0f;
+    for (i = 1; i < (N - 1); i++) {
+        t = 0 + i * step;
+        // P3x * (...) + P5x * (...) + R2x * (...) + R3x * (...)
+        // curve 2: P3 es P5 pontokon megy at, P4 es P6 az erintok szamolasahoz vannak
+        HermiteCurve2[i * 3] = Points[3*2] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[3*4] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3*1] * (t * t * t - 2.0f * t * t + t) + Vectors[3*2] * (t * t * t - t * t);
+        HermiteCurve2[i * 3 + 1] = Points[3*2+1] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[3*4+1] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3*1+1] * (t * t * t - 2.0f * t * t + t) + Vectors[3*2+1] * (t * t * t - t * t);
+        HermiteCurve2[i * 3 + 2] = 0.0f;
+    }
+    // P5 pontba vegzodik a gorbe
+    HermiteCurve2[(3 * N) - 3] = Points[3*4];
+    HermiteCurve2[(3 * N) - 2] = Points[3*4+1];
+    HermiteCurve2[(3 * N) - 1] = 0.0f;
+}
+void calcThird() {
+    // masodik gorbe ujraszamolasa, P3 pontbol indul a gorbe
+    HermiteCurve3[0] = Points[3 * 4];
+    HermiteCurve3[1] = Points[3 * 4 + 1];
+    HermiteCurve3[2] = 0.0f;
+    for (i = 1; i < (N - 1); i++) {
+        t = 0 + i * step;
+        // P3x * (...) + P5x * (...) + R2x * (...) + R3x * (...)
+        // curve 2: P3 es P5 pontokon megy at, P4 es P6 az erintok szamolasahoz vannak
+        HermiteCurve3[i * 3] = Points[3 * 4] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[3 * 6] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3 * 2] * (t * t * t - 2.0f * t * t + t) + Vectors[3 * 3] * (t * t * t - t * t);
+        HermiteCurve3[i * 3 + 1] = Points[3 * 4 + 1] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[3 * 6 + 1] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3 * 2 + 1] * (t * t * t - 2.0f * t * t + t) + Vectors[3 * 3 + 1] * (t * t * t - t * t);
+        HermiteCurve3[i * 3 + 2] = 0.0f;
+    }
+    // P5 pontba vegzodik a gorbe
+    HermiteCurve3[(3 * N) - 3] = Points[3 * 6];
+    HermiteCurve3[(3 * N) - 2] = Points[3 * 6 + 1];
+    HermiteCurve3[(3 * N) - 1] = 0.0f;
+}
+
 void cursorPosCallback(GLFWwindow* window, double x, double y) {
     // ha valamelyik pontot mozgatjuk, azaz valamelyiket drageljuk, 
     // az eger kurzor pontjaival felulirom az akt. pontom koordinatajat
@@ -75,28 +133,12 @@ void cursorPosCallback(GLFWwindow* window, double x, double y) {
         
         // elso vagy masodik pontot fogjuk meg -> csak az elso gorbe valtozik
         if (dragged == 0 || dragged == 1) {
-            // elso erinto v ujraszamolasa
-            Vectors[0] = Points[3] - Points[0];
-            Vectors[1] = Points[4] - Points[1];
+            // elso erinto v ujraszamolasa P2 - P1
+            Vectors[3*0] = Points[3*1] - Points[3*0];
+            Vectors[3*0+1] = Points[3*1+1] - Points[3*0+1]; 
 
             // elso gorbe ujraszamolasa
-            // P1 pontbol indul a gorbe
-            HermiteCurve[0] = Points[0];
-            HermiteCurve[1] = Points[1];
-            HermiteCurve[2] = 0.0f;
-
-            for (i = 1; i < (N - 1); i++) {
-                t = 0 + i * step;
-                // P1x * (...) + P3x * (...) + R1x * (...) + R2x * (...)
-                // P1 és P3 pontokon megy át a görbe, P2 és P4 csak az érintővektorok számolásához vannak
-                HermiteCurve[i * 3] = Points[0] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[6] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[0] * (t * t * t - 2.0f * t * t + t) + Vectors[3] * (t * t * t - t * t);
-                HermiteCurve[i * 3 + 1] = Points[1] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[7] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[1] * (t * t * t - 2.0f * t * t + t) + Vectors[4] * (t * t * t - t * t);
-                HermiteCurve[i * 3 + 2] = 0.0f;
-            }
-            // P3 pontba vegzodik a gorbe
-            HermiteCurve[(3 * N) - 3] = Points[6];
-            HermiteCurve[(3 * N) - 2] = Points[7];
-            HermiteCurve[(3 * N) - 1] = 0.0f;
+            calcFirst();
 
             // 1. vbo frissitese
             glBindBuffer(GL_ARRAY_BUFFER, vbo1);
@@ -107,40 +149,12 @@ void cursorPosCallback(GLFWwindow* window, double x, double y) {
         // harmadik vagy negyedik pontot fogjuk meg
         // azaz kozos pontot fogunk meg, tehat mindket gorbe valtozik!
         if (dragged == 2 || dragged == 3) {
-            // masodik erinto v ujraszamolasa
-            Vectors[3] = Points[9] - Points[6];
-            Vectors[4] = Points[10] - Points[7];
+            // masodik erinto v ujraszamolasa P4 - P3
+            Vectors[3*1] = Points[3*3] - Points[3*2]; 
+            Vectors[3*1+1] = Points[3*3+1] - Points[3*2+1];
 
-            // elso gorbe P1-bol indul
-            HermiteCurve[0] = Points[0];
-            HermiteCurve[1] = Points[1];
-            HermiteCurve[2] = 0.0f;
-            // masodik gorbe P3-bol indul
-            HermiteCurve2[0] = Points[6];
-            HermiteCurve2[1] = Points[7];
-            HermiteCurve2[2] = 0.0f;
-
-            for (i = 1; i < (N - 1); i++) {
-                t = 0 + i * step;
-                // P1x * (...) + P3x * (...) + R1x * (...) + R2x * (...)
-                // P1 és P3 pontokon megy át a görbe, P2 és P4 csak az érintővektorok számolásához vannak
-                HermiteCurve[i * 3] = Points[0] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[6] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[0] * (t * t * t - 2.0f * t * t + t) + Vectors[3] * (t * t * t - t * t);
-                HermiteCurve[i * 3 + 1] = Points[1] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[7] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[1] * (t * t * t - 2.0f * t * t + t) + Vectors[4] * (t * t * t - t * t);
-                HermiteCurve[i * 3 + 2] = 0.0f;
-
-                HermiteCurve2[i * 3] = Points[6] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[12] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3] * (t * t * t - 2.0f * t * t + t) + Vectors[6] * (t * t * t - t * t);
-                HermiteCurve2[i * 3 + 1] = Points[7] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[13] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[4] * (t * t * t - 2.0f * t * t + t) + Vectors[7] * (t * t * t - t * t);
-                HermiteCurve2[i * 3 + 2] = 0.0f;
-            }
-            // P3 pontba vegzodik az elso gorbe
-            HermiteCurve[(3 * N) - 3] = Points[6];
-            HermiteCurve[(3 * N) - 2] = Points[7];
-            HermiteCurve[(3 * N) - 1] = 0.0f;
-
-            // P5 pontba vegzodik a masodik gorbe
-            HermiteCurve2[(3 * N) - 3] = Points[12];
-            HermiteCurve2[(3 * N) - 2] = Points[13];
-            HermiteCurve2[(3 * N) - 1] = 0.0f;
+            calcFirst();
+            calcSecond();
 
             // 1. es 2. vbo frissitese
             glBindBuffer(GL_ARRAY_BUFFER, vbo1);
@@ -151,37 +165,39 @@ void cursorPosCallback(GLFWwindow* window, double x, double y) {
             glBufferData(GL_ARRAY_BUFFER, (3 * N) * sizeof(GLfloat), HermiteCurve2, GL_STATIC_DRAW);
             glBindBuffer(GL_ARRAY_BUFFER, 0);
         }
-        // otodik vagy hatodik pontot fogjuk meg -> csak a masodik gorbe valtozik
+        // otodik vagy hatodik pontot fogjuk meg -> masodik es harmadik gorbe valtozik
         if (dragged == 4 || dragged == 5) {
-            // masodik erinto v ujraszamolasa
-            Vectors[6] = Points[15] - Points[12];
-            Vectors[7] = Points[16] - Points[13];
+            // harmadik erinto v ujraszamolasa
+            Vectors[3*2] = Points[3*5] - Points[3*4];
+            Vectors[3*2+1] = Points[3*5+1] - Points[3*4+1];
 
-            // masodik gorbe ujraszamolasa
-            // P3 pontbol indul a gorbe
-            HermiteCurve2[0] = Points[6];
-            HermiteCurve2[1] = Points[7];
-            HermiteCurve2[2] = 0.0f;
+            calcSecond();
+            calcThird();
 
-            for (i = 1; i < (N - 1); i++) {
-                t = 0 + i * step;
-                HermiteCurve2[i * 3] = Points[6] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[12] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3] * (t * t * t - 2.0f * t * t + t) + Vectors[6] * (t * t * t - t * t);
-                HermiteCurve2[i * 3 + 1] = Points[7] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[13] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[4] * (t * t * t - 2.0f * t * t + t) + Vectors[7] * (t * t * t - t * t);
-                HermiteCurve2[i * 3 + 2] = 0.0f;
-            }
-            // P5 pontba vegzodik a gorbe
-            HermiteCurve2[(3 * N) - 3] = Points[12];
-            HermiteCurve2[(3 * N) - 2] = Points[13];
-            HermiteCurve2[(3 * N) - 1] = 0.0f;
-
-            // 2. vbo frissitese
+            // 2. es 3. vbo frissitese
             glBindBuffer(GL_ARRAY_BUFFER, vbo2);
             glBufferData(GL_ARRAY_BUFFER, (3 * N) * sizeof(GLfloat), HermiteCurve2, GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+            glBindBuffer(GL_ARRAY_BUFFER, vbo3);
+            glBufferData(GL_ARRAY_BUFFER, (3 * N) * sizeof(GLfloat), HermiteCurve3, GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+        }
+        // hetedik vagy nyolcadik pontot fogjuk meg -> harmadik gorbe valtozik
+        if (dragged == 6 || dragged == 7) {
+            // negyedik erinto v ujraszamolasa
+            Vectors[3*3] = Points[3*7] - Points[3*6];
+            Vectors[3*3+1] = Points[3*7+1] - Points[3*6+1];
+
+            calcThird();
+            // 3. vbo frissitese
+            glBindBuffer(GL_ARRAY_BUFFER, vbo3);
+            glBufferData(GL_ARRAY_BUFFER, (3 * N) * sizeof(GLfloat), HermiteCurve3, GL_STATIC_DRAW);
             glBindBuffer(GL_ARRAY_BUFFER, 0);
         }
         // points vbo frissitese
         glBindBuffer(GL_ARRAY_BUFFER, pointsvbo);
-        glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(GLfloat), Points, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), Points, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     }
@@ -249,7 +265,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Drag&Drop", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "HermiteProject", NULL, NULL);
     if (!window) {
         fprintf(stderr, "ERROR: could not open window with GLFW3\n");
         glfwTerminate();
@@ -267,38 +283,10 @@ int main() {
     glfwSetCursorPosCallback(window, cursorPosCallback);
     glfwSetMouseButtonCallback(window, mouseButtonCallback);
 
-    // curve 1 kezdo
-    HermiteCurve[0] = Points[0];
-    HermiteCurve[1] = Points[1];
-    HermiteCurve[2] = 0.0f;
-    // curve 2 kezdo
-    HermiteCurve2[0] = Points[6];
-    HermiteCurve2[1] = Points[7];
-    HermiteCurve2[2] = 0.0f;
-
-    for (i = 1; i < (N - 1); i++) {
-        t = 0 + i * step;
-        // P1x * (...) + P3x * (...) + R1x * (...) + R2x * (...)
-        // P1 és P3 pontokon megy át a görbe, P2 és P4 csak az érintővektorok számolásához vannak
-        HermiteCurve[i * 3] =     Points[0] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[6] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[0] * (t * t * t - 2.0f * t * t + t) + Vectors[3] * (t * t * t - t * t);
-        HermiteCurve[i * 3 + 1] = Points[1] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[7] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[1] * (t * t * t - 2.0f * t * t + t) + Vectors[4] * (t * t * t - t * t);
-        HermiteCurve[i * 3 + 2] = 0.0f;
-
-        // P3x * (...) + P5x * (...) + R2x * (...) + R3x * (...)
-        // curve 2: P3 es P5 pontokon megy at, P4 es P6 az erintok szamolasahoz vannak
-        HermiteCurve2[i * 3] = Points[6] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[12] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[3] * (t * t * t - 2.0f * t * t + t) + Vectors[6] * (t * t * t - t * t);
-        HermiteCurve2[i * 3 + 1] = Points[7] * (2.0f * t * t * t - 3.0f * t * t + 1) + Points[13] * (-2.0f * t * t * t + 3.0f * t * t) + Vectors[4] * (t * t * t - 2.0f * t * t + t) + Vectors[7] * (t * t * t - t * t);
-        HermiteCurve2[i * 3 + 2] = 0.0f;
-    }
-    // P3 pontba vegzodik az 1. curve
-    HermiteCurve[(3 * N) - 3] = Points[6];
-    HermiteCurve[(3 * N) - 2] = Points[7];
-    HermiteCurve[(3 * N) - 1] = 0.0f;
-
-    // P5 pontba vegzodik a 2. curve
-    HermiteCurve2[(3 * N) - 3] = Points[12];
-    HermiteCurve2[(3 * N) - 2] = Points[13];
-    HermiteCurve2[(3 * N) - 1] = 0.0f;
+    // elso es masodik gorbe kiszamolasa
+    calcFirst();
+    calcSecond();
+    calcThird();
 
     // Curve 1 vbo
     glGenBuffers(1, &vbo1);
@@ -308,14 +296,19 @@ int main() {
     glGenBuffers(1, &vbo2);
     glBindBuffer(GL_ARRAY_BUFFER, vbo2);
     glBufferData(GL_ARRAY_BUFFER, (3 * N) * sizeof(GLfloat), HermiteCurve2, GL_STATIC_DRAW);
+    // Curve 3 vbo
+    glGenBuffers(1, &vbo3);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo3);
+    glBufferData(GL_ARRAY_BUFFER, (3 * N) * sizeof(GLfloat), HermiteCurve3, GL_STATIC_DRAW);
 
     glGenBuffers(1, &pointsvbo);
     glBindBuffer(GL_ARRAY_BUFFER, pointsvbo);
-    glBufferData(GL_ARRAY_BUFFER, (18) * sizeof(GLfloat), Points, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, (24) * sizeof(GLfloat), Points, GL_STATIC_DRAW);
 
     glGenVertexArrays(1, &vao1); // Curve 1
     glGenVertexArrays(1, &pointsvao);
     glGenVertexArrays(1, &vao2); // Curve 2
+    glGenVertexArrays(1, &vao3); // Curve 3
 
     glBindVertexArray(vao1);
     glEnableVertexAttribArray(0);
@@ -325,6 +318,11 @@ int main() {
     glBindVertexArray(vao2);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glBindVertexArray(vao3);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo3);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
     glBindVertexArray(pointsvao);
@@ -375,7 +373,7 @@ int main() {
         // pontok
         glUseProgram(shader_programme2);
         glBindVertexArray(pointsvao);
-        glDrawArrays(GL_POINTS, 0, 6);
+        glDrawArrays(GL_POINTS, 0, 8);
 
         // hermite curve 1
         glUseProgram(shader_programme1);
@@ -387,12 +385,17 @@ int main() {
         glBindVertexArray(vao2);
         glDrawArrays(GL_LINE_STRIP, 0, N);
 
+        // hermite curve 3
+        glUseProgram(shader_programme1);
+        glBindVertexArray(vao3);
+        glDrawArrays(GL_LINE_STRIP, 0, N);
+
         // erintok
         glUseProgram(shader_programme3);
         glBindVertexArray(pointsvao);
         glDrawArrays(GL_LINE_STRIP, 0, 2);
 
-        // masodik kettot kotjuk ossze
+        // masodik erinto
         glUseProgram(shader_programme3);
         glBindVertexArray(pointsvao);
         glDrawArrays(GL_LINE_STRIP, 2, 2); // masodik kettot
@@ -401,6 +404,11 @@ int main() {
         glUseProgram(shader_programme3);
         glBindVertexArray(pointsvao);
         glDrawArrays(GL_LINE_STRIP, 4, 2);
+
+        // negyedik erinto
+        glUseProgram(shader_programme3);
+        glBindVertexArray(pointsvao);
+        glDrawArrays(GL_LINE_STRIP, 6, 2);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
