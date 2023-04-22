a5a04b147f90b7e7e10e405f634af173b6b6dea1
Hosszú Szilárd <55355512+mentol555@users.noreply.github.com>
2023-04-22 23:40:54 +0300

    Negyzet pontok korre lecserelese

--- hermit_erintok_beadando.cpp
+++ hermit_erintok_beadando.cpp
@@ -6,14 +6,17 @@
 #define WIDTH 960
 #define HEIGHT 960
 
+#define PI 3.14159265359
+#define RADIUS 0.02f
 #define N 100
+#define no_of_points 8
 
 using namespace std;
 
 GLuint vao1, vao2, vao3;
-GLuint pointsvao;
+GLuint pointsvao, circlesvao;
 GLuint vbo1, vbo2, vbo3; // Hermit curves vbos
-GLuint pointsvbo;
+GLuint pointsvbo, circlesvbo;
 
 GLfloat HermiteCurve[4 * N];
 GLfloat HermiteCurve2[4 * N]; // masodik hermit gorbe
@@ -23,8 +26,15 @@ GLint dragged = -1; // tarolja h melyik pontot fogtuk meg
 
 GLfloat t;
 GLint i;
-
+// hermite step
 GLfloat step = 1.0f / (N - 1); // t in [0,1]
+
+// korokhoz tartozo dolgok=
+GLfloat circles[no_of_points * 303];
+GLfloat tt;
+GLint ii;
+// circles step
+GLfloat circlestep = (2 * PI) / 99;
 
 GLfloat Points[] = {
         -0.75f, 0.0f, 0.0f, // elso pontja
@@ -56,7 +66,7 @@ GLfloat dist2_2d(GLfloat P1x, GLfloat P1y, GLfloat P2x, GLfloat P2y) {
 GLint getActivePoint(GLfloat* p, GLfloat sensitivity, GLfloat x, GLfloat y) {
 
     GLfloat		s = sensitivity * sensitivity; // sugar negyzet
-    GLfloat		xNorm = -1 + x / (WIDTH / 2); // eger pozicio normalizalasa: 640x640-esbol [-1,1] rendszerbe
+    GLfloat		xNorm = -1 + x / (WIDTH / 2); // eger pozicio normalizalasa: 960x960-asbol [-1,1] rendszerbe
     GLfloat		yNorm = -1 + (HEIGHT - y) / (HEIGHT / 2);
 
     for (GLint i = 0; i < 8; i++)
@@ -119,6 +129,26 @@ void calcThird() {
     HermiteCurve3[(3 * N) - 2] = Points[3 * 6 + 1];
     HermiteCurve3[(3 * N) - 1] = 0.0f;
 }
+void calccircles(int index) {
+    // elso kor: 0-302 es koordok, masodik kor:303-605-os koordok...
+    // korok kozeppontjai a pontok, az adott indexen
+    // dragged = 0 -> index = 0 -> centerIndex = 100*0+0 = 0 -> X:0, Y:1: Z:2
+    // dragged = 1 -> index = 1 -> centerIndex = 100*1+1 = 1 -> X:3*101-> 303, Y:304, Z:305
+    int centerIndex = 100 * index + (index);
+    circles[centerIndex * 3] = Points[index*3];
+    circles[centerIndex * 3 + 1] = Points[index*3 + 1];
+    circles[centerIndex * 3 + 2] = Points[index*3 + 2];
+
+    // dragged = 0 -> index = 0 -> centerIndex = 0 -> first = 1
+    // dragged = 1 -> index = 1 -> centerIndex = 101 -> first = 102 ...
+    int first = centerIndex+1;
+    for (ii = first; ii < first+100; ii++) {
+        tt = ii * circlestep;
+        circles[ii * 3] = Points[index*3] + RADIUS * cos(tt);
+        circles[ii * 3 + 1] = Points[index*3 + 1] + RADIUS * sin(tt);
+        circles[ii * 3 + 2] = 0.0f;
+    }
+}
 
 void cursorPosCallback(GLFWwindow* window, double x, double y) {
     // ha valamelyik pontot mozgatjuk, azaz valamelyiket drageljuk, 
@@ -130,6 +160,12 @@ void cursorPosCallback(GLFWwindow* window, double x, double y) {
 
         Points[3 * dragged] = xNorm;  // x coord
         Points[3 * dragged + 1] = yNorm;  // y coord
+
+        // mozgatott kor ujraszamolasa
+        calccircles(dragged);
+        glBindBuffer(GL_ARRAY_BUFFER, circlesvbo);
+        glBufferData(GL_ARRAY_BUFFER, (no_of_points * 303) * sizeof(GLfloat), circles, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
         
         // elso vagy masodik pontot fogjuk meg -> csak az elso gorbe valtozik
         if (dragged == 0 || dragged == 1) {
@@ -197,7 +233,7 @@ void cursorPosCallback(GLFWwindow* window, double x, double y) {
         }
         // points vbo frissitese
         glBindBuffer(GL_ARRAY_BUFFER, pointsvbo);
-        glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(GLfloat), Points, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, (3*no_of_points) * sizeof(GLfloat), Points, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     }
@@ -283,10 +319,19 @@ int main() {
     glfwSetCursorPosCallback(window, cursorPosCallback);
     glfwSetMouseButtonCallback(window, mouseButtonCallback);
 
+    // kor szamolasok
+    for (int i = 0; i < no_of_points; i++) {
+        calccircles(i);
+    }
     // elso es masodik gorbe kiszamolasa
     calcFirst();
     calcSecond();
     calcThird();
+
+    // circles vbo
+    glGenBuffers(1, &circlesvbo);
+    glBindBuffer(GL_ARRAY_BUFFER, circlesvbo);
+    glBufferData(GL_ARRAY_BUFFER, (no_of_points*303) * sizeof(GLfloat), circles, GL_STATIC_DRAW);
 
     // Curve 1 vbo
     glGenBuffers(1, &vbo1);
@@ -300,15 +345,22 @@ int main() {
     glGenBuffers(1, &vbo3);
     glBindBuffer(GL_ARRAY_BUFFER, vbo3);
     glBufferData(GL_ARRAY_BUFFER, (3 * N) * sizeof(GLfloat), HermiteCurve3, GL_STATIC_DRAW);
-
+    // Points vbo
     glGenBuffers(1, &pointsvbo);
     glBindBuffer(GL_ARRAY_BUFFER, pointsvbo);
-    glBufferData(GL_ARRAY_BUFFER, (24) * sizeof(GLfloat), Points, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, (3*no_of_points) * sizeof(GLfloat), Points, GL_STATIC_DRAW);
 
+    // -------------------VAOS -----------------------------
+    glGenVertexArrays(1, &circlesvao); // circles
     glGenVertexArrays(1, &vao1); // Curve 1
-    glGenVertexArrays(1, &pointsvao);
+    glGenVertexArrays(1, &pointsvao); // Points
     glGenVertexArrays(1, &vao2); // Curve 2
     glGenVertexArrays(1, &vao3); // Curve 3
+
+    glBindVertexArray(circlesvao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, circlesvbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
     glBindVertexArray(vao1);
     glEnableVertexAttribArray(0);
@@ -330,6 +382,7 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, pointsvbo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
+    // -------------- SHADER PART ------------------
     vert_shader1 = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vert_shader1, 1, &vertex_shader1, NULL);
     glCompileShader(vert_shader1);
@@ -364,16 +417,28 @@ int main() {
     GLuint vert_shader, frag_shader;
     GLuint shader_programme;
 
-
     glPointSize(15.0f);
 
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // pontok
+        // pontok kirajz
+        //glUseProgram(shader_programme2);
+        //glBindVertexArray(pointsvao);
+        //glDrawArrays(GL_POINTS, 0, 8);
+
+        // korok kirajz
         glUseProgram(shader_programme2);
-        glBindVertexArray(pointsvao);
-        glDrawArrays(GL_POINTS, 0, 8);
+        glBindVertexArray(circlesvao);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 101);
+        glDrawArrays(GL_TRIANGLE_FAN, 102, 101);
+        glDrawArrays(GL_TRIANGLE_FAN, 203, 101);
+        glDrawArrays(GL_TRIANGLE_FAN, 304, 101);
+        glDrawArrays(GL_TRIANGLE_FAN, 405, 101);
+        glDrawArrays(GL_TRIANGLE_FAN, 506, 101);
+        glDrawArrays(GL_TRIANGLE_FAN, 607, 101);
+        glDrawArrays(GL_TRIANGLE_FAN, 708, 101);
 
         // hermite curve 1
         glUseProgram(shader_programme1);
@@ -394,20 +459,11 @@ int main() {
         glUseProgram(shader_programme3);
         glBindVertexArray(pointsvao);
         glDrawArrays(GL_LINE_STRIP, 0, 2);
-
         // masodik erinto
-        glUseProgram(shader_programme3);
-        glBindVertexArray(pointsvao);
         glDrawArrays(GL_LINE_STRIP, 2, 2); // masodik kettot
-
         // harmadik erinto
-        glUseProgram(shader_programme3);
-        glBindVertexArray(pointsvao);
         glDrawArrays(GL_LINE_STRIP, 4, 2);
-
         // negyedik erinto
-        glUseProgram(shader_programme3);
-        glBindVertexArray(pointsvao);
         glDrawArrays(GL_LINE_STRIP, 6, 2);
 
         glfwPollEvents();
